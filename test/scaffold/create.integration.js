783b7cd0d4fa24d979c82b080ef1e71abdca3f16
Cofresi <cofresi@dash.org>
2021-03-01 21:53:15 -0400

    chore: bump version and deps (#78)
    * chore: bump version and deps
    
    * chore: update package-lock.json
    
    * fix: tests

--- test/scaffold/create.integration.js
+++ test/scaffold/create.integration.js
@@ -71,8 +71,7 @@ describe('#create', function() {
       should.equal(fs.existsSync(packagePath), true);
 
       var config = JSON.parse(fs.readFileSync(configPath));
-      config.services.should.deep.equal(['dashd', 'db', 'address', 'web']);
-      config.datadir.should.equal('./data');
+      config.services.should.deep.equal(['dashd', 'web']);
       config.network.should.equal('livenet');
 
       var pack = JSON.parse(fs.readFileSync(packagePath));
